827f8d55cb70dc257ca7bbe7cedcc05b7e8d0e2b
mk-pmb <mk-pmb@users.noreply.github.com>
2024-11-22 02:35:04 +0100

    [test] Fix timezone offset test.

--- README.md
+++ README.md
@@ -17,14 +17,15 @@ from [test.usage.js](test.usage.js):
 
 <!--#include file="test.usage.js" outdent="  " code="javascript"
   start="  // #BEGIN# usage demo" stop="  // #ENDOF# usage demo" -->
-<!--#verbatim lncnt="12" -->
+<!--#verbatim lncnt="13" -->
 ```javascript
-var date2iso = require('date2iso'), tzo = detectTimeZone();
+var date2iso = require('date2iso'), tzo;
 
 equal(date2iso(0), '1970-01-01T00:00:00.000Z');
 equal(date2iso().slice(0, 5), (new Date()).getFullYear() + '-');
 
-equal(date2iso(2018, 8, 23, 12 + tzo.hr, 34 + tzo.min, 56, 789),
+tzo = getTimeZoneOffsetHourForDayAtNoon(2018, 8, 23);
+equal(date2iso(2018, 8, 23, 12 + tzo, 34, 56, 789),
               '2018-09-23T12:34:56.789Z');
 
 equal(date2iso('2018-09-23 12:34:56 UTC+0200'),
--- test.usage.js
+++ test.usage.js
@@ -3,21 +3,23 @@
 'use strict';
 
 require('usnam-pmb');
-var equal = require('assert').strictEqual;
+var assert = require('assert'), equal = assert.strictEqual;
 
-function detectTimeZone() {
-  var o = -(new Date()).getTimezoneOffset(), m = o % 60, h = (o - m) / 60;
-  return { hr: h, min: m };
+function getTimeZoneOffsetHourForDayAtNoon(y, m, d) {
+  var o = -(new Date(y, m, d, 12, 0, 0)).getTimezoneOffset();
+  return Math.round(o / 60);
+  // Sorry if you're living in a timezone with minute offsets.
 }
 
 (function readmeDemo() {
   // #BEGIN# usage demo
-  var date2iso = require('date2iso'), tzo = detectTimeZone();
+  var date2iso = require('date2iso'), tzo;
 
   equal(date2iso(0), '1970-01-01T00:00:00.000Z');
   equal(date2iso().slice(0, 5), (new Date()).getFullYear() + '-');
 
-  equal(date2iso(2018, 8, 23, 12 + tzo.hr, 34 + tzo.min, 56, 789),
+  tzo = getTimeZoneOffsetHourForDayAtNoon(2018, 8, 23);
+  equal(date2iso(2018, 8, 23, 12 + tzo, 34, 56, 789),
                 '2018-09-23T12:34:56.789Z');
 
   equal(date2iso('2018-09-23 12:34:56 UTC+0200'),
